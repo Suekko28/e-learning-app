f935b3e738bae9a71a843bf387df60d003b8b815
solehiskandar <132457694+solehiskandar@users.noreply.github.com>
2023-11-27 19:04:46 +0700

    try

--- resources/views/layout/app-admin.blade.php
+++ resources/views/layout/app-admin.blade.php
@@ -145,7 +145,6 @@
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
     @stack('scripts')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @yield('navbar-admin')
 </body>
 
